40dd74027e0267d35216eab206d944596a365dae
BuildTools <unconfigured@null.spigotmc.org>
2025-02-28 15:54:50 +0100

    Ajout de tests pour la création d'un livre

--- TP-TDD-Bibliotheque-Test/BookControllerTests.cs
+++ TP-TDD-Bibliotheque-Test/BookControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
+using System.Text;
 using TP_TDD_Bibliotheque.Models;
 
 namespace TP_TDD_Bibliotheque_Test
@@ -12,12 +13,80 @@ namespace TP_TDD_Bibliotheque_Test
             _client = factory.CreateClient();
         }
 
+        private static string GenerateValidISBN10()
+        {
+            Random random = new Random();
+            int[] digits = new int[9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                digits[i] = random.Next(0, 10);
+            }
+
+            int total = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                total += digits[i] * (10 - i);
+            }
+
+            int remainder = total % 11;
+            int checkDigit = (11 - remainder) % 11;
+
+            StringBuilder isbn = new StringBuilder();
+            foreach (var digit in digits)
+            {
+                isbn.Append(digit);
+            }
+
+            isbn.Append(checkDigit == 10 ? 'X' : checkDigit.ToString());
+
+            return isbn.ToString();
+        }
+
+        private static string GenerateValidISBN13()
+        {
+            Random random = new Random();
+            int[] isbn = new int[12];
+
+            for (int i = 0; i < 12; i++)
+            {
+                isbn[i] = random.Next(0, 10);
+            }
+
+            int total = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                total += (i % 2 == 0) ? isbn[i] : isbn[i] * 3;
+            }
+
+            int remainder = total % 10;
+            int checkDigit = (remainder == 0) ? 0 : 10 - remainder;
+
+            return string.Concat(isbn.Select(d => d.ToString())) + checkDigit;
+        }
+
         [Fact]
-        public async Task CreateBook_ShouldReturn201Created()
+        public async Task CreateBookWithValidIsbn13_ShouldReturn201Created()
         {
             Book book = new Book
             {
-                ISBN = "0201896834",
+                ISBN = GenerateValidISBN13(),
+                Title = "The Art of Computer Programming",
+                Available = true,
+                EditorId = 1
+            };
+
+            var response = await _client.PostAsJsonAsync("/book", book);
+
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreateBookWithValidIsbn10_ShouldReturn201Created()
+        {
+            Book book = new Book
+            {
+                ISBN = GenerateValidISBN10(),
                 Title = "The Art of Computer Programming",
                 Available = true,
                 EditorId = 1
@@ -45,6 +114,91 @@ namespace TP_TDD_Bibliotheque_Test
                 Title = "The Art of Computer Programming",
                 Available = true,
                 EditorId = 999
+            };
+            var response = await _client.PostAsJsonAsync("/book", book);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreateBook_ShouldReturn400BadRequest_WhenISBNIsNullOrEmpty()
+        {
+            Book book = new Book
+            {
+                ISBN = "",
+                Title = "The Art of Computer Programming",
+                Available = true,
+                EditorId = 1
+            };
+            var response = await _client.PostAsJsonAsync("/book", book);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreateBook_ShouldReturn400BadRequest_WhenTitleIsNullOrEmpty()
+        {
+            Book book = new Book
+            {
+                ISBN = "0201896834",
+                Title = "",
+                Available = true,
+                EditorId = 1
+            };
+            var response = await _client.PostAsJsonAsync("/book", book);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreateBook_ShouldReturn400BadRequest_WhenISBNAlreadyExists()
+        {
+            Book book = new Book
+            {
+                ISBN = "0201896834",
+                Title = "The Art of Computer Programming",
+                Available = true,
+                EditorId = 1
+            };
+            await _client.PostAsJsonAsync("/book", book);
+            var response = await _client.PostAsJsonAsync("/book", book);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreateBook_ShouldReturn400BadRequest_WhenEditorIdIsZero()
+        {
+            Book book = new Book
+            {
+                ISBN = "0201896834",
+                Title = "The Art of Computer Programming",
+                Available = true,
+                EditorId = 0
+            };
+            var response = await _client.PostAsJsonAsync("/book", book);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreateBook_ShouldReturn400BadRequest_WhenIsbn10IsInvalid()
+        {
+            Book book = new Book
+            {
+                ISBN = "01234567895",
+                Title = "The Art of Computer Programming",
+                Available = true,
+                EditorId = 1
+            };
+            var response = await _client.PostAsJsonAsync("/book", book);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreateBook_ShouldReturn400BadRequest_WhenIsbn13IsInvalid()
+        {
+            Book book = new Book
+            {
+                ISBN = "9781234567895",
+                Title = "The Art of Computer Programming",
+                Available = true,
+                EditorId = 1
             };
             var response = await _client.PostAsJsonAsync("/book", book);
             Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
--- TP-TDD-Bibliotheque/Controllers/BookController.cs
+++ TP-TDD-Bibliotheque/Controllers/BookController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TP_TDD_Bibliotheque.Models;
 
 namespace TP_TDD_Bibliotheque.Controllers;
@@ -22,6 +23,40 @@ public class BookController : ControllerBase
         var editor = await _context.Editors.FindAsync(book.EditorId);
         if (editor == null) return BadRequest("L'éditeur n'existe pas");
 
+        if (string.IsNullOrEmpty(book.ISBN) || string.IsNullOrEmpty(book.Title))
+        {
+            return BadRequest("Le livre est invalide");
+        }
+
+        if (book.ISBN.Length == 13)
+        {
+            if (!CheckISBN13(book.ISBN))
+            {
+                return BadRequest("L'ISBN-13 est invalide");
+            }
+        }
+        else if (book.ISBN.Length == 10)
+        {
+            if (!CheckISBN10(book.ISBN))
+            {
+                return BadRequest("L'ISBN-10 est invalide");
+            }
+        }
+        else
+        {
+            return BadRequest("L'ISBN n'a pas un taille valide");
+        }
+
+        if (await _context.Books.AnyAsync(b => b.ISBN == book.ISBN))
+        {
+            return BadRequest("Le livre existe déjà");
+        }
+
+        if (book.EditorId == 0)
+        {
+            return BadRequest("L'éditeur est invalide");
+        }
+
         _context.Books.Add(book);
         await _context.SaveChangesAsync();
 
@@ -40,4 +75,51 @@ public class BookController : ControllerBase
 
         return Ok(book);
     }
+
+    private static bool CheckISBN13(string isbn)
+    {
+        int total = 0;
+        for (int i = 0; i < 13; i++)
+        {
+            int digit = isbn[i] - '0';
+
+            if (i % 2 == 0)
+            {
+                total += digit;
+            }
+            else
+            {
+                total += digit * 3;
+            }
+        }
+
+        return total % 10 == 0;
+    }
+
+    private static bool CheckISBN10(string isbn)
+    {
+        int total = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            if (!char.IsDigit(isbn[i]))
+            {
+                if (i == 9 && isbn[i] == 'X')
+                {
+                    total += 10 * (10 - i);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                total += (isbn[i] - '0') * (10 - i);
+            }
+        }
+
+        return total % 11 == 0;
+    }
+
+
 }
